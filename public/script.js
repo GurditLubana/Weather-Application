331b5ff58787b41dc8f3a39dfa8b4ee767fdf878
GurditLubana <singhg78@myumanitoba.ca>
2022-12-22 04:42:39 -0600

    initial commit

---
+++ public/script.js
@@ -0,0 +1,14 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f450c78fb7msh8765990528797f3p1cda5ejsn15acb181d563',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Winnipeg', options)
+	.then(response => response.json())
+	.then(response => {console.log(response)
+	
+	document.getElementById("nameId").innerHTML = "hello sir"})
+	.catch(err => console.error(err));
